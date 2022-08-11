c653f3b70db0c86cbec9490f86e9bc6212c4effe
wenyiyimo <81872056+wenyiyimo@users.noreply.github.com>
2022-08-11 19:53:42 +0800

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,18 @@
+<script>
+import init from '@/utils/init.js';
+export default {
+	onLaunch: function() {
+		init.init();
+	},
+	onShow: function() {
+		console.log('App Show');
+	},
+	onHide: function() {
+		console.log('App Hide');
+	}
+};
+</script>
+<style lang="scss">
+/*每个页面公共css */
+@import url('@/common/common.scss');
+</style>
